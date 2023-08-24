09e1775ca12fbad8f4942600f1b04370e766977f
Greg Johnston <gregmj.sco@gmail.com>
2023-08-24 13:53:18 +0900

    Fix webpack config file name and generate home gafe from module

---
+++ src/home.js
@@ -0,0 +1,31 @@
+import './style.css';
+
+function hero__title_component() {
+    const hero__title = document.createElement('h1');
+    hero__title.innerHTML = "Gyu-tan Restaurant Ouji";
+    hero__title.classList.add('hero__title');
+
+    return hero__title;
+}
+
+function hero__img_component() {
+    const hero__img = document.createElement('img');
+    hero__img.src = "https://live.staticflickr.com/563/32650990614_dc2dc9753b_z.jpg";
+    hero__img.classList.add('hero__img');
+
+    return hero__img;
+}
+
+function hero__description_component() {
+    const hero__description = document.createElement('p');
+    hero__description.innerHTML = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi in porro inventore, reiciendis eius architecto itaque dolor, sit rerum maxime provident at. Reiciendis sequi, culpa harum accusamus fugiat dolorem odit.";
+    hero__description.classList.add('hero__description');
+
+    return hero__description;
+}
+
+export {
+    hero__title_component,
+    hero__img_component,
+    hero__description_component
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("TEST");
+import './style.css';
+
+import {hero__title_component,
+    hero__img_component,
+    hero__description_component} from './home';
+
+function component() {
+    const element = document.createElement('div');
+ 
+    element.id ="content";
+    element.appendChild(hero__title_component());
+    element.appendChild(hero__img_component());
+    element.appendChild(hero__description_component())
+ 
+    return element;
+  }
+
+document.body.appendChild(component());
